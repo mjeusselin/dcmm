3511a9a4eee610e14197f796ec1253f1de80cdad
Maxime Jeusselin <maxime.jeusselin@outlook.com>
2019-09-29 16:20:02 +0200

    initial commit

---
+++ dcmm-parent/src/main/java/fr/mjeu/dcmm/DcmManagerApplication.java
@@ -0,0 +1,35 @@
+package fr.mjeu.dcmm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DcmManagerApplication {
+	
+	static Logger logger = LoggerFactory.getLogger(DcmManagerApplication.class);
+	
+	@Value( "${data.element.tag}" )
+	private String dataElementTag;
+	
+	@Value( "${data.value.field}" )
+	private String dataValueField;
+	
+	public static void main(String[] args) {
+        SpringApplication.run(DcmManagerApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            logger.info("Launching Dicom Manager Application with parameters : ");
+            logger.info("data.element.tag="+dataElementTag);
+            logger.info("data.value.field="+dataValueField);
+        };
+    }
+}
